013c9cb7ebdaa0e2e7fdeb954d04eb8b188ce96b
minhdang051202 <142491361+minhdang051202@users.noreply.github.com>
2024-06-14 22:03:01 +0700

    baicau2

--- obj/Debug/net8.0/NMD-2021050190.AssemblyInfo.cs
+++ obj/Debug/net8.0/NMD-2021050190.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NMD-2021050190")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c136e5788ae25668212b507111f6e55e84ff48")]
 [assembly: System.Reflection.AssemblyProductAttribute("NMD-2021050190")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NMD-2021050190")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
